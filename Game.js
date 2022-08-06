7df0a4bd494dc1c3576debf495f7875d7093da5c
Louis <33809479+LouisPetrik@users.noreply.github.com>
2022-08-06 16:40:59 +0200

    Game klasse zum speichern von wichtigen informationen für den spielstand angelegt

---
+++ Game.js
@@ -0,0 +1,11 @@
+class Game {
+  // so far, benötigen wir nichts im konstruktor
+  constructor() {}
+
+  static weißAmZug = true
+  static posWeißerKing = 'e1'
+  static posSchwarzerKing = 'e8'
+
+  static weißHatRochiert = false
+  static schwarzHatRochiert = false
+}
