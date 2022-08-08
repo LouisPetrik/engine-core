6737857967782f2c814c063e9b2ecd16b80d847f
LouisPetrik <louispetrik88@gmail.com>
2022-08-08 19:40:05 +0200

    berechnen der springer züge die möglich sind funktioniert super, tests bisher erfüllt - manche möglichen zielfelder werden doppelt generiert, ist aber nicht schlimm, aber noch zu fixxen

--- figuren/Knight.js
+++ figuren/Knight.js
@@ -14,20 +14,34 @@ export function moeglicheZuegeKnight(ausgangsfeldKoord, brettState, weißAmZug) 
 	//i - 1, j + 2
 	//i + 1, j - 2
 	//i + 1, j + 2
-	//i + 2, j - 2
-	//i + 2, j + 2
+	//i + 2, j - 1
+	//i + 2, j + 1
+
+	// für schwarz kommt noch hinzu (gehen ebenfalls für weiß)
+	// i + 2, j - 1
+	// i + 2, j + 1
 
 	// überprüft ob die Koordinaten des Zielfeldes für den Springer legitim sind (noch auf dem Brett)
 	// bekommt dann auch noch den check ob dort eine eigene figur ist
-	function koordinatenLegitim(iTemp, jTemp) {
-		if (iTemp >= 0 && jTemp >= 0 && iTemp <= 7 && jTemp <= 7) {
+	function zielfeldLegitim(iVariation, jVariation) {
+		// ob koordinate überhaupt verfügbar ist auf dem brett
+		if (
+			iVariation >= 0 &&
+			jVariation >= 0 &&
+			iVariation <= 7 &&
+			jVariation <= 7
+		) {
 			if (weißAmZug) {
+				// insofern dort keine eigene Figur steht, wird true returned, damit ist das Zielfeld legitim
 				return !['Q', 'P', 'R', 'N', 'B', 'K'].includes(
-					brettState[i - 2][j - 1]
+					brettState[iVariation][jVariation]
 				)
 			}
 
 			if (!weißAmZug) {
+				return !['q', 'p', 'r', 'n', 'b', 'k'].includes(
+					brettState[iVariation][jVariation]
+				)
 			}
 		} else {
 			return false
@@ -35,85 +49,53 @@ export function moeglicheZuegeKnight(ausgangsfeldKoord, brettState, weißAmZug) 
 	}
 
 	// Der Springer wurde von weiß gezogen:
-	if (weißAmZug) {
-		// testen, ob mögliche L-Zug Felder durch eigene Figuren belegt sind. Falls nicht, zu den möglichen Zügen adden
-		// und ob die mögliche position nicht außerhalb des spielfeldes ist.
-		// kann eleganteren ansatz geben, hinsichtlich rekursion und der funktion für das navigieren über felder - rewrite!
-		// muss alles in funktion umgeschrieben werden!
 
-		if (
-			koordinatenLegitim(i - 2, j - 1) &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i - 2][j - 1])
-		) {
-			zuege.push([i - 2, j - 1])
-		}
+	// testen, ob mögliche L-Zug Felder durch eigene Figuren belegt sind. Falls nicht, zu den möglichen Zügen adden
+	// und ob die mögliche position nicht außerhalb des spielfeldes ist.
+	// kann eleganteren ansatz geben, hinsichtlich rekursion und der funktion für das navigieren über felder - rewrite!
+	// muss alles in funktion umgeschrieben werden!
 
-		if (
-			koordinatenLegitim(i - 2, j + 1) &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i - 2][j + 1])
-		) {
-			zuege.push([i - 2, j + 1])
-		}
+	if (zielfeldLegitim(i - 2, j - 1)) {
+		zuege.push([i - 2, j - 1])
+	}
 
-		if (
-			i - 1 >= 0 &&
-			i - 1 <= 7 &&
-			j - 2 >= 0 &&
-			j - 2 <= 7 &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i - 1][j - 2])
-		) {
-			zuege.push([i - 1, j - 2])
-		}
+	if (zielfeldLegitim(i - 2, j + 1)) {
+		zuege.push([i - 2, j + 1])
+	}
 
-		if (
-			i - 1 >= 0 &&
-			i - 1 <= 7 &&
-			j + 2 >= 0 &&
-			j + 2 <= 7 &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i - 1][j + 2])
-		) {
-			zuege.push([i - 1, j + 2])
-		}
+	if (zielfeldLegitim(i - 1, j - 2)) {
+		zuege.push([i - 1, j - 2])
+	}
 
-		if (
-			i + 1 >= 0 &&
-			i + 1 <= 7 &&
-			j - 2 >= 0 &&
-			j - 2 <= 7 &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i + 1][j - 2])
-		) {
-			zuege.push([i + 1, j - 2])
-		}
+	if (zielfeldLegitim(i - 1, j + 2)) {
+		zuege.push([i - 1, j + 2])
+	}
 
-		if (
-			i + 1 >= 0 &&
-			i + 1 <= 7 &&
-			j + 2 >= 0 &&
-			j + 2 <= 7 &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i + 1][j + 2])
-		) {
-			zuege.push([i + 1, j + 2])
-		}
+	if (zielfeldLegitim(i + 1, j - 2)) {
+		zuege.push([i + 1, j - 2])
+	}
 
-		if (
-			i + 2 >= 0 &&
-			i + 2 <= 7 &&
-			j - 2 >= 0 &&
-			j - 2 <= 7 &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i + 2][j - 2])
-		) {
-			zuege.push([i + 2, j - 2])
-		}
+	if (zielfeldLegitim(i + 1, j + 2)) {
+		zuege.push([i + 1, j + 2])
+	}
 
-		if (
-			i + 2 >= 0 &&
-			i + 2 <= 7 &&
-			j + 2 >= 0 &&
-			j + 2 <= 7 &&
-			!['Q', 'P', 'R', 'N', 'B', 'K'].includes(brettState[i + 2][j + 2])
-		) {
-			zuege.push([i + 2, j + 2])
-		}
+	if (zielfeldLegitim(i + 2, j - 1)) {
+		zuege.push([i + 2, j - 1])
+	}
+
+	if (zielfeldLegitim(i + 2, j + 1)) {
+		zuege.push([i + 2, j + 1])
+	}
+
+	// i + 2, j - 1
+	// i + 2, j + 1
+	// die zusätzlichen für die beiden schwarzen springer:
+	if (zielfeldLegitim(i + 2, j - 1)) {
+		zuege.push([i + 2, j - 1])
+	}
+
+	if (zielfeldLegitim(i + 2, j + 1)) {
+		zuege.push([i + 2, j + 1])
 	}
 
 	// Der Springer wurde von schwarz gezogen
